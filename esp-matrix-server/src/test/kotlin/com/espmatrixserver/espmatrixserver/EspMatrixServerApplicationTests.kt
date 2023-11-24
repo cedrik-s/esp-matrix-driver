968c24c39ea4ae2716ea9dc6ed760cf3c8337549
LeanderBockholt <42433110+LeanderBockholt@users.noreply.github.com>
2023-11-24 22:57:30 +0100

    initial commit spring/kotlin

---
+++ esp-matrix-server/src/test/kotlin/com/espmatrixserver/espmatrixserver/EspMatrixServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.espmatrixserver.espmatrixserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EspMatrixServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
