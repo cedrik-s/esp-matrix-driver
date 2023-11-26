192fbf852eadac7c71a55d9c92f5f4fc1c8a7ee3
Cedrik Schmiedinghoff <cedrik.schmiedinghoff@gmail.com>
2023-11-26 01:21:45 +0100

    use okhttp instead of RestClient and use HTTP1.1

--- esp-matrix-server/src/main/kotlin/com/espmatrixserver/espmatrixserver/client/ESPHttpJsonClient.kt
+++ esp-matrix-server/src/main/kotlin/com/espmatrixserver/espmatrixserver/client/ESPHttpJsonClient.kt
@@ -2,26 +2,47 @@ package com.espmatrixserver.espmatrixserver.client
 
 import com.espmatrixserver.espmatrixserver.dto.BitmapDTO
 import com.espmatrixserver.espmatrixserver.dto.drawDTO
+import com.fasterxml.jackson.databind.ObjectMapper
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import org.springframework.web.client.RestClient
+
 
 @Service
 class ESPHttpJsonClient(espConfiguration: ESPConfiguration) {
-    val baseUrl = "http://" + espConfiguration.ip + ":" + espConfiguration.port
-    val restClient = RestClient.create()
-    fun drawBitMap(bitmap: BitmapDTO) {
-        var response = restClient.post()
-            .uri("$baseUrl/bitmap")
-            .body(bitmap)
-            .retrieve()
-            .toEntity(String::class.java)
 
+    private val logger: Logger = LoggerFactory.getLogger(this.javaClass)
+
+    val baseUrl = "http://" + espConfiguration.ip + ":" + espConfiguration.port
+
+    val client = OkHttpClient.Builder()
+        .protocols(listOf(Protocol.HTTP_1_1))
+        .build();
+
+    fun drawBitMap(bitmap: BitmapDTO) {
+        val reqBody = ObjectMapper().writeValueAsString(bitmap).toRequestBody("application/json".toMediaType())
+        var response = client.newCall(
+            Request.Builder()
+                .url("$baseUrl/bitmap")
+                .post(reqBody)
+                .build()
+        ).execute()
+
+        logger.info(response.toString());
     }
     fun drawLine(x0: Short, y0: Short, x1: Short, y1: Short, color: UShort) {
-        var response = restClient.post()
-            .uri("$baseUrl/drawLine")
-            .body(drawDTO(x0, y0, x1, y1, color))
-            .retrieve()
-            .toEntity(String::class.java)
+        val reqBody = ObjectMapper().writeValueAsString(drawDTO(x0, y0, x1, y1, color)).toRequestBody("application/json".toMediaType())
+        var response = client.newCall(
+            Request.Builder()
+                .url("$baseUrl/drawLine")
+                .post(reqBody)
+                .build()
+        ).execute()
+
+        logger.info(response.toString());
     }
 }
