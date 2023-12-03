beaf95e5fb5802c9f077eec4df487b9edca32165
LeanderBockholt <42433110+LeanderBockholt@users.noreply.github.com>
2023-12-03 22:28:40 +0100

    add Logging for AlphaVantageDisplayingModule

--- esp-matrix-server/src/main/kotlin/com/espmatrixserver/espmatrixserver/DisplayingModules/AlphaVantageDisplayingModule.kt
+++ esp-matrix-server/src/main/kotlin/com/espmatrixserver/espmatrixserver/DisplayingModules/AlphaVantageDisplayingModule.kt
@@ -4,11 +4,20 @@ import com.espmatrixserver.espmatrixserver.SpringContext
 import com.espmatrixserver.espmatrixserver.client.AlphaVantageClient
 import com.espmatrixserver.espmatrixserver.dto.DrawDTO
 import com.espmatrixserver.espmatrixserver.service.GenerateGraphService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class AlphaVantageDisplayingModule : DisplayingModule {
+    private val logger: Logger = LoggerFactory.getLogger(this.javaClass)
+
     override fun generateFrame(): List<DrawDTO> {
         val result = SpringContext.getBean(AlphaVantageClient::class.java).getTimeSeries("TSLA")
         val stockUnits = result?.stockUnits
+        if(stockUnits?.size==0){
+            logger.warn("StockUnits are empty")
+            logger.warn(result?.errorMessage)
+            return emptyList();
+        }
         return if (stockUnits == null) {
             throw Exception("StockUnits are null")
         } else {
